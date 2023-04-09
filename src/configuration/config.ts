9a1062ac5a970388f3b14c37785895d0ff98bf50
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-04-09 08:34:53 +0200

    Configured test and dev envs

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -3,14 +3,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    password: process.env.PASSWORD as string,
+    database_prod: process.env.DATABASE_PROD,
+    host_prod: process.env.HOST_PROD,
+    password_prod: process.env.PASSWORD_PROD as string,
+    db_port_prod: process.env.DB_PORT_PROD,
+    user_prod: process.env.USER_PROD,
+
+    database_test: process.env.DATABASE_TEST,
+    host_test: process.env.HOST_TEST,
+    password_test: process.env.PASSWORD_TEST as string,
+    db_port_test: process.env.DB_PORT_TEST,
+    user_test: process.env.USER_TEST,
+
+    database_dev: process.env.DATABASE_DEV,
+    host_dev: process.env.HOST_DEV,
+    password_dev: process.env.PASSWORD_DEV as string,
+    db_port_dev: process.env.DB_PORT_DEV,
+    user_dev: process.env.USER_DEV,
+
     port: process.env.PORT,
-    db_port: process.env.DB_PORT,
-    user: process.env.USER,
     salt: Number(process.env.SALT),
     pepper: process.env.PEPPER,
     secret_key: process.env.SECRET_KEY as string,
-    ssl:process.env.SSLMODE
+    ssl: process.env.SSLMODE,
+    ENV: process.env.ENV
 }
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,14 +2,36 @@ import { Pool } from 'pg';
 import { config } from '../configuration/config';
 import fs from 'fs';
 
-export const client = new Pool({
-    database: config.database,
-    host: config.host,
-    user: config.user,
-    password: config.password as string,
-    port: Number(config.db_port),
-    // ssl:{
-    //     cert: fs.readFileSync('../../ssl/BaltimoreCyberTrustRoot.crt.pem'),
-    //     rejectUnauthorized: true,
-    //   },
-});
+let client: Pool;
+
+if (config.ENV === 'prod') {
+    client = new Pool({
+        database: config.database_prod,
+        host: config.host_prod,
+        user: config.user_prod,
+        password: config.password_prod as string,
+        port: Number(config.db_port_prod),
+        // ssl:{
+        //     cert: fs.readFileSync('../../ssl/BaltimoreCyberTrustRoot.crt.pem'),
+        //     rejectUnauthorized: true,
+        //   },
+    });
+} else if (config.ENV === 'test') {
+    client = new Pool({
+        database: config.database_test,
+        host: config.host_test,
+        user: config.user_test,
+        password: config.password_test as string,
+        port: Number(config.db_port_test),
+    });
+} else { //dev
+    client = new Pool({
+        database: config.database_dev,
+        host: config.host_dev,
+        user: config.user_dev,
+        password: config.password_dev as string,
+        port: Number(config.db_port_dev),
+    });
+}
+
+export default client;
