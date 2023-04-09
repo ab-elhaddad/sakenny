b6bd50ed128b2a2b31e2441e752fb249c4c8156a
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-04-09 11:30:09 +0200

    Creted function to store images on cloud

---
+++ src/models/storeImages.ts
@@ -0,0 +1,28 @@
+import cloudinary from '../cloudinairy/config';
+
+const storeImages = async (images: string[], folder: string): Promise<string[]> => {
+    let urls: string[] = [];
+
+    images.forEach(async (image: string) => {
+        const options = {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: true,
+            folder: folder,
+            transformation: [
+                { width: 1000, crop: "scale" },
+                { quality: "auto" },
+                { fetch_format: "auto" }
+            ]
+        }
+
+        const res = await cloudinary.uploader.upload(image, options);
+
+        console.log(res);
+        urls.push(res.secure_url);
+    });
+
+    return urls;
+}
+
+export default storeImages;
