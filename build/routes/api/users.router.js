8aa5d3cbe8099390e19d1453f71ad877a83cad4a
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-05-15 02:58:09 +0300

    add last commit to build

--- build/routes/api/users.router.js
+++ build/routes/api/users.router.js
@@ -8,9 +8,9 @@ const users_controller_1 = require("../../controllers/users.controller");
 const authenticate_middleware_1 = __importDefault(require("../../middlewares/authenticate.middleware"));
 const usersRouter = (app) => {
     app.post('/users/register', users_controller_1.register);
-    app.get('/users/login', users_controller_1.login);
+    app.post('/users/login', users_controller_1.login);
     app.put('/users/reset-password', users_controller_1.resetPassword);
-    app.get('/users/profile', users_controller_1.profile);
+    app.post('/users/profile', users_controller_1.profile);
     app.put('/users/update', authenticate_middleware_1.default, users_controller_1.update);
     app.put('/users/update-password', authenticate_middleware_1.default, users_controller_1.updatePassword);
 };
