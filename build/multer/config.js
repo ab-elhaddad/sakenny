97867695eb056f44e3ab71a35915944a236f2f87
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-05-17 21:01:10 +0300

    Removed __dirname from storage path

--- build/multer/config.js
+++ build/multer/config.js
@@ -5,10 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fileStorage = void 0;
 const multer_1 = __importDefault(require("multer"));
-const path_1 = __importDefault(require("path"));
 exports.fileStorage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path_1.default.join(__dirname, '../images/'));
+        cb(null, '../images/');
     },
     filename: (req, file, cb) => {
         //console.log(file);
--- src/multer/config.ts
+++ src/multer/config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../images/'));
+        cb(null, '../images/');
     },
     filename: (req, file, cb) => {
         //console.log(file);
