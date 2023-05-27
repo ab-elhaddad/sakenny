5ebcdd655556cd552edb6fc887d0db116887b0d2
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-05-27 06:46:19 +0300

    Updated ads table schema

--- migrations/sqls/20230406035517-ads-up.sql
+++ migrations/sqls/20230406035517-ads-up.sql
@@ -1,17 +1,19 @@
 CREATE TABLE IF NOT EXISTS ads
 (
     id SERIAL PRIMARY KEY,
-    users_id INTEGER REFERENCES users(id),
+    user_id INTEGER REFERENCES users(id) NOT NULL,
     title VARCHAR(50) NOT NULL,
     space_type VARCHAR(4) NOT NULL,
     description VARCHAR(500),
     price REAL NOT NULL,
     city VARCHAR(20) NOT NULL,
-    address VARCHAR(100) NOT NULL,
+    governorate VARCHAR(20) NOT NULL,
     lat VARCHAR(255), 
     lng VARCHAR(255), 
     gender BOOLEAN NOT NULL,
-    services TEXT[],
-    pics TEXT[],
-    price_per VARCHAR(5) NOT NULL
+    features BIT(3),
+    terms BIT(3),
+    price_per VARCHAR(5) NOT NULL,
+    email VARCHAR(100),
+    phone_number VARCHAR(20) NOT NULL
 );
