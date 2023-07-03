ff4c9db0896eb8e3ab7636c25a6c0f7ec3e2ca19
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-07-03 22:00:10 +0300

    Removed useless function

--- build/types/serchedAd.type.js
+++ build/types/serchedAd.type.js
@@ -1,15 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkSearchedAd = void 0;
-const checkSearchedAd = (ad) => {
-    let flag = false;
-    flag || (flag = ad.governorate != undefined);
-    flag || (flag = ad.city != undefined);
-    flag || (flag = ad.space_type != undefined);
-    flag || (flag = ad.start_price != undefined);
-    flag || (flag = ad.end_price != undefined);
-    flag || (flag = ad.features != undefined);
-    flag || (flag = ad.terms != undefined);
-    return flag;
-};
-exports.checkSearchedAd = checkSearchedAd;
