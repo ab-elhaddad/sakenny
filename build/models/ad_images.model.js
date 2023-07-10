df0246aefe4d5e0ee5129f854c273ca5e98cf6cd
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-07-10 04:26:54 +0300

    Added some changes to AdImages model

--- build/models/ad_images.model.js
+++ build/models/ad_images.model.js
@@ -16,54 +16,63 @@ const index_1 = __importDefault(require("../database/index"));
 class AdImages {
     constructor() {
         this.deleteImage = (ad_id, image_url, user) => __awaiter(this, void 0, void 0, function* () {
-            const connection = yield index_1.default.connect();
-            // Get user id 
-            const sqlToGetUserId = 'SELECT id FROM users WHERE email=$1 or phone_number=$1';
-            const user_id = (yield connection.query(sqlToGetUserId, [user])).rows[0].id;
-            // Check if the ad belongs to the user
-            const sqlCheckAd = 'SELECT * FROM ads WHERE id=($1) and user_id=($2)';
-            const check = (yield connection.query(sqlCheckAd, [ad_id, user_id])).rowCount > 0;
-            if (!check) {
-                connection.release();
-                return { Message: "No such ad with the provided token id", Flag: false };
+            try {
+                const connection = yield index_1.default.connect();
+                // Get user id 
+                const sqlToGetUserId = 'SELECT id FROM users WHERE email=$1 or phone_number=$1';
+                const user_id = (yield connection.query(sqlToGetUserId, [user])).rows[0].id;
+                // Check if the ad belongs to the user
+                const sqlCheckAd = 'SELECT * FROM ads WHERE id=($1) and user_id=($2)';
+                const check = (yield connection.query(sqlCheckAd, [ad_id, user_id])).rowCount > 0;
+                if (!check) {
+                    connection.release();
+                    return { Message: "No such ad with the provided token id", Flag: false };
+                }
+                else {
+                    const sql = 'DELETE FROM ad_images WHERE ad_id=($1) and url=($2)';
+                    const res = yield connection.query(sql, [ad_id, image_url]);
+                    connection.release();
+                    if (res.rowCount === 0)
+                        return { Message: "No such image with the provided url", Flag: false };
+                    else
+                        return { Message: "Image deleted successfully", Flag: true };
+                }
             }
-            else {
-                const sql = 'DELETE FROM ad_images WHERE ad_id=($1) and url=($2)';
-                const res = yield connection.query(sql, [ad_id, image_url]);
-                connection.release();
-                if (res.rowCount === 0)
-                    return { Message: "No such image with the provided url", Flag: false };
-                else
-                    return { Message: "Image deleted successfully", Flag: true };
+            catch (e) {
+                //console.log('Error in deleteImage in ad_images.model\n', e);
+                return { Message: "Something went wrong!", Flag: false };
             }
         });
         this.addImage = (ad_id, image_url, image_description) => __awaiter(this, void 0, void 0, function* () {
             try {
-                console.log(ad_id, image_url, image_description);
+                //console.log(ad_id, image_url, image_description);
                 const connection = yield index_1.default.connect();
                 const sql = 'INSERT INTO ad_images (ad_id, url, description) VALUES ($1, $2, $3) RETURNING *';
                 const res = yield connection.query(sql, [ad_id, image_url, image_description]);
-                console.log(res.rows[0]);
+                //console.log(res.rows[0]);
                 if (res.rowCount > 0)
                     return { Message: "Image added successfully", Flag: true };
                 else
                     return { Message: "Something went wrong!", Flag: false };
             }
             catch (e) {
-                console.log('Error in addImage in ad_images.model\n', e);
+                //console.log('Error in addImage in ad_images.model\n', e);
                 return { Message: "Something went wrong!", Flag: false };
             }
         });
         this._deleteImage = (url) => __awaiter(this, void 0, void 0, function* () {
             try {
                 const connection = yield index_1.default.connect();
-                const sql = 'DELETE FROM ad_images WHERE url=($1)';
-                yield connection.query(sql, [url]);
+                const sql = 'DELETE FROM ad_images WHERE url=($1) RETURNING *';
+                const ans = yield connection.query(sql, [url]);
                 connection.release();
-                return { Flag: true };
+                if (ans.rowCount === 0)
+                    return { Flag: false };
+                else
+                    return { Flag: true };
             }
             catch (e) {
-                console.log('Error in _deleteImage in ad_images.model\n', e);
+                //console.log('Error in _deleteImage in ad_images.model\n', e);
                 return { Flag: false };
             }
         });
--- src/models/ad_images.model.ts
+++ src/models/ad_images.model.ts
@@ -2,46 +2,52 @@ import client from "../database/index";
 
 class AdImages {
     deleteImage = async (ad_id: number, image_url: string, user: string) => {
-        const connection = await client.connect();
+        try {
+            const connection = await client.connect();
 
-        // Get user id 
-        const sqlToGetUserId = 'SELECT id FROM users WHERE email=$1 or phone_number=$1';
-        const user_id = (await connection.query(sqlToGetUserId, [user])).rows[0].id;
+            // Get user id 
+            const sqlToGetUserId = 'SELECT id FROM users WHERE email=$1 or phone_number=$1';
+            const user_id = (await connection.query(sqlToGetUserId, [user])).rows[0].id;
 
-        // Check if the ad belongs to the user
-        const sqlCheckAd = 'SELECT * FROM ads WHERE id=($1) and user_id=($2)';
-        const check: boolean = (await connection.query(sqlCheckAd, [ad_id, user_id])).rowCount > 0;
+            // Check if the ad belongs to the user
+            const sqlCheckAd = 'SELECT * FROM ads WHERE id=($1) and user_id=($2)';
+            const check: boolean = (await connection.query(sqlCheckAd, [ad_id, user_id])).rowCount > 0;
 
-        if (!check) {
-            connection.release();
-            return { Message: "No such ad with the provided token id", Flag: false };
+            if (!check) {
+                connection.release();
+                return { Message: "No such ad with the provided token id", Flag: false };
+            }
+            else {
+                const sql = 'DELETE FROM ad_images WHERE ad_id=($1) and url=($2)';
+                const res = await connection.query(sql, [ad_id, image_url]);
+                connection.release();
+
+                if (res.rowCount === 0)
+                    return { Message: "No such image with the provided url", Flag: false };
+                else
+                    return { Message: "Image deleted successfully", Flag: true };
+            }
         }
-        else {
-            const sql = 'DELETE FROM ad_images WHERE ad_id=($1) and url=($2)';
-            const res = await connection.query(sql, [ad_id, image_url]);
-            connection.release();
-
-            if (res.rowCount === 0)
-                return { Message: "No such image with the provided url", Flag: false };
-            else
-                return { Message: "Image deleted successfully", Flag: true };
+        catch (e) {
+            //console.log('Error in deleteImage in ad_images.model\n', e);
+            return { Message: "Something went wrong!", Flag: false };
         }
     }
 
     addImage = async (ad_id: number, image_url: string, image_description: string) => {
         try {
-            console.log(ad_id, image_url, image_description);
+            //console.log(ad_id, image_url, image_description);
             const connection = await client.connect();
             const sql = 'INSERT INTO ad_images (ad_id, url, description) VALUES ($1, $2, $3) RETURNING *'
             const res = await connection.query(sql, [ad_id, image_url, image_description]);
-            console.log(res.rows[0]);
+            //console.log(res.rows[0]);
             if (res.rowCount > 0)
                 return { Message: "Image added successfully", Flag: true };
             else
                 return { Message: "Something went wrong!", Flag: false };
         }
         catch (e) {
-            console.log('Error in addImage in ad_images.model\n', e);
+            //console.log('Error in addImage in ad_images.model\n', e);
             return { Message: "Something went wrong!", Flag: false };
         }
     }
@@ -49,13 +55,17 @@ class AdImages {
     _deleteImage = async (url: string) => {
         try {
             const connection = await client.connect();
-            const sql = 'DELETE FROM ad_images WHERE url=($1)';
-            await connection.query(sql, [url]);
+            const sql = 'DELETE FROM ad_images WHERE url=($1) RETURNING *';
+            const ans = await connection.query(sql, [url]);
             connection.release();
-            return { Flag: true };
+
+            if (ans.rowCount === 0)
+                return { Flag: false };
+            else
+                return { Flag: true };
         }
         catch (e) {
-            console.log('Error in _deleteImage in ad_images.model\n', e);
+            //console.log('Error in _deleteImage in ad_images.model\n', e);
             return { Flag: false };
         }
     }
