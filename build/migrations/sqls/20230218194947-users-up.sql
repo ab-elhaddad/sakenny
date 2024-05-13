8d80633fac20ec7ce4fc85d9215bf0711953e0be
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2024-05-13 03:51:23 +0300

    fix: deleted build dir

--- build/migrations/sqls/20230218194947-users-up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users
-(
-    id SERIAL PRIMARY KEY,
-    fullname VARCHAR(75) NOT NULL,
-    email VARCHAR(100),
-    phone_number VARCHAR(20),
-    password VARCHAR(255) NOT NULL
-);
--- build/routes/api/admins.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const admins_controller_1 = require("../../controllers/admins.controller");
-const adminsRouter = (app) => {
-    app.post('/admins/sign-up', admins_controller_1.signUp);
-    app.post('/admins/sign-in', admins_controller_1.signIn);
-};
-exports.default = adminsRouter;
